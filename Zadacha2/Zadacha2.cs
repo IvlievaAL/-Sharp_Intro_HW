36b0e01579818fbd651b3ab100ca892e0e35068c
AlexIv <ivlieva.medphyslab.com>
2023-02-11 13:57:40 +0300

    Two numbers entered

--- Zadacha2/Zadacha2.cs
+++ Zadacha2/Zadacha2.cs
@@ -3,3 +3,9 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
+Console.WriteLine("Enter number1: ");
+string StrNumber1 = Console.ReadLine();
+int number1 = Convert.ToInt32(StrNumber1);
+Console.WriteLine("Enter number2: ");
+string StrNumber2 = Console.ReadLine();
+int number2 = Convert.ToInt32(StrNumber2);
